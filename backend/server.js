19774b7135e66c06117fec244a0bf66f441f326c
Gems <developer.gemjoshua@gmail.com>
2022-07-23 09:03:44 +0100

    ready for deploy

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ require("colors");
 const { errorHandler } = require("./middlewares/errorMiddleware");
 const dbConn = require("./configs/db");
 const PORT = process.env.PORT || 7000;
+const path = require("path");
 
 //CONN TO DB
 dbConn();
@@ -19,6 +20,20 @@ app.use(errorHandler);
 //ROUTE
 app.use("/api/goals", require("./routes/goalsRoute"));
 app.use("/api/users", require("./routes/userRoute"));
+
+//PRODUCTION ONLY
+if (process.env.NODE_ENV === "production") {
+  //set static asset
+  app.use(express.static(path.join(__dirname, "../client/build")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(
+      path.resolve(__dirname, "../", "client", "build", "index.html")
+    );
+  });
+} else {
+  app.get("*", (req, res) => res.send("Please run app in production"));
+}
 
 //LISTEN TO SERVER
 app.listen(PORT, () => console.log("Server Now Listening On Port ", PORT));
